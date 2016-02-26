c990dc8a363a2e6e2e304956e625f6544c73d7f9
GregoryElliott <gregoryelliott34@yahoo.com>
2016-02-26 14:49:33 -0600

    parse out ingredients and quantities from a recipe

--- Team6/recipe_api.py
+++ Team6/recipe_api.py
@@ -1,9 +1,37 @@
-'''Version 0.1'''
+import requests
+from bs4 import BeautifulSoup
+import re
 
 def autograder(url):
     '''Accepts the URL for a recipe, and returns a dictionary of the
     parsed results in the correct format. See project sheet for
     details on correct format.'''
     # your code here
+    results = {}
+    results['url'] = url
+    results['ingredients'] = []
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    ingredients = soup.findAll("span", { "itemprop" : "ingredients" })
+    quantity_reg = re.compile(r'[\d/]+ (?:teaspoon|tablespoon|cup)?s?')
+    for ingregient_expression in ingredients:
+    	ingredient_dict = {}
+    	print ingregient_expression
+    	quantity = re.findall(quantity_reg, ingregient_expression.contents[0])
+    	if len(quantity) > 1:
+    		ingredient = ingregient_expression.contents[0].replace(quantity[0], "")
+    		ingredient_dict['quantity'] = quantity[0]
+    	else:
+    		ingredient = ingregient_expression.contents[0]
+    		ingredient_dict['quantity'] = 1
+    	ingredient_dict['ingredient'] = ingredient
+    	print ingredient_dict
+    	results['ingredients'].append(ingredient_dict)
 
     return results
+
+def main():
+	print autograder('http://allrecipes.com/recipe/80827/easy-garlic-broiled-chicken/')
+
+if __name__ == '__main__':
+    main()
